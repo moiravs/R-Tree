d402c85e43e592b1dbb7888a2e80ff56e2d97c49
emily <moira.vanderslagmolen@ulb.be>
2023-03-18 17:28:44 +0100

    [INIT] code

---
+++ projet.java
@@ -0,0 +1,5 @@
+import org.opengis.feature.simple.SimpleFeature;
+import org.locationtech.jts.geom.MultiPolygon;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.Envelope;
+import org.geotools.geometry.jts.GeometryBuilder;
